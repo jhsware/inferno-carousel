43b8a917863b24f8e42a8611e573f5ab38b69014
jhsware <sebastian@urbantalk.se>
2019-02-15 20:29:22 +0100

    Moved utilities to inferno-move which also needed it since that package is a requirement anyway

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,44 +1,5 @@
-const ARR = [];
-
-export const Children = {
-  map(children, fn, ctx) {
-    if (isNullOrUndef(children)) {
-      return children;
-    }
-    children = Children.toArray(children);
-    if (ctx && ctx !== children) {
-      fn = fn.bind(ctx);
-    }
-    return children.map(fn);
-  },
-  count(children) {
-    children = Children.toArray(children);
-    return children.length;
-  },
-  only(children) {
-    children = Children.toArray(children);
-    if (children.length !== 1) {
-      throw new Error('Children.only() expects only one child.');
-    }
-    return children[0];
-  },
-  toArray(children) {
-    if (isNullOrUndef(children)) {
-      return [];
-    }
-    // We need to flatten arrays here,
-    // because React does it also and application level code might depend on that behavior
-    if (isArray(children)) {
-      const result = [];
-
-      flatten(children, result);
-
-      return result;
-    }
-    return ARR.concat(children);
-  }
-}
 import { cloneVNode } from 'inferno-clone-vnode'
+export { Children } from 'inferno-move'
 
 export const addEvent = function(elem, type, eventHandle) {
   if (elem === null || typeof elem === 'undefined') {
